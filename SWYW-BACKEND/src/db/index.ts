70495f5e69d472c5047b3df19531b813e0ac293f
programadorisgod <catapiascastro@gmail.com>
2025-09-25 19:44:23 -0500

    update DATABASE URL variable

--- SWYW-BACKEND/src/db/index.ts
+++ SWYW-BACKEND/src/db/index.ts
@@ -1,4 +1,4 @@
-import { DATABASE_URL } from '@src/config/app.config';
+import { DB_URL } from '@src/config/app.config';
 import { drizzle } from 'drizzle-orm/postgres-js';
 
-export const db = drizzle(DATABASE_URL);
+export const db = drizzle(DB_URL);
